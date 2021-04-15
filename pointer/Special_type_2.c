4e9146e6c913d9f56f3b097ec02481b4a9d7e09d
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-04-15 21:13:43 +0530

    Add files via upload

---
+++ pointer/Special_type_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int *test()
+{
+    static int x[4];
+    for (int i = 0; i < 4; i++)
+    {
+        x[i] = i % 2;
+    }
+    return x;
+}
+int main()
+{
+    int *arr = test();
+    printf("%d", *(arr + 3));
+    return 0;
+}
