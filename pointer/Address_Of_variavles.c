f61ebef463b01f7389a4ebf6fc992c7ff532a4ef
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-04-15 21:12:01 +0530

    Add files via upload

---
+++ pointer/Address_Of_variavles.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// void pointer(int x);
+int main(){
+    int a=130;
+    int b=130;
+    int c=130;
+    printf("Address of a is %x.\n",&a);
+    printf("Address of b is %x.\n",&b);
+    printf("Address of c is %x.\n",&c);
+    // pointer();
+    return 0;
+}
