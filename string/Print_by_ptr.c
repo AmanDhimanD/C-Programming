fd4d5a55a7754494357a570ccf187a556939cf4b
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-04-25 23:44:08 +0530

    Add files via upload

---
+++ string/Print_by_ptr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char ch[] = "Aman dhiman ", *ptr;
+    ptr = ch; //store the base address of string "ch"
+
+    int i = 0;
+
+    while (ch[i] != '\0') //'\0' is use to terminate the string; ! is make condiotion false, if con is false then it print the value else empty
+    {
+        printf("%c", ch[i]);
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
