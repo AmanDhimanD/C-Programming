ed60a99dae79f08f45df172fe5936d583a238420
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-04-26 20:28:04 +0530

    Add files via upload

---
+++ string/Reverse_lib.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+//reverse the string value.......
+int main()
+{
+    char str1[100] = ".noitanretnecnoc gnirts si sihT";
+    strrev(str1);
+    printf("%s", str1);
+    return 0;
+}
