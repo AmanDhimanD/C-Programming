58e65f7943015709ee223a1fc2911e24c768441c
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-04-29 12:23:42 +0530

    Add files via upload

---
+++ while loops/while.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a = 900;
+
+    printf("VIP number of JIO....\n");
+
+    while (a<1000)
+    {
+        printf("\n9999999%d.",a);
+        a++;
+    }
+    // both are same with For loops and While loops
+
+    // for (int a = 1; a <= 10; a++)
+    // {
+    //     printf("\nNumber is %d.",a);
+    // }
+        return 0;
+}
