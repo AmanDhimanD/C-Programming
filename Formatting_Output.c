d802cf17d6319de2534c7582c5de8ff98fa08b86
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-04-30 20:22:35 +0530

    Add files via upload

---
+++ Formatting_Output.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+
+    printf("Age For Voting is %d.", 18);
+
+    //if we want to print a quotes in page then do this step
+
+    printf("\nAre You greater than 18?,\n \"If Yes Then Congratulation to you Buddy. \"\n");
+
+    return 0;
+}
+ 
