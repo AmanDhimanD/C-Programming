fb9626fc64c727e3320a4d33a2d47ff1ae106c41
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-05-01 17:05:58 +0530

    Add files via upload

---
+++ if.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int marks = 82;
+    /*if (expression)
+        statements */
+    if (marks > 33);
+    //double %% is use for print % in printf
+    printf("Congratulation,Your are passed with %d %%.", marks);
+
+    return 0;
+}
