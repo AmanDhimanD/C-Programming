f0769347db649dcdca555bb38d3b7dd867d412ae
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-05-04 19:41:00 +0530

    Add files via upload

---
+++ logical.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int n=23;
+
+ 
+    if (n > 0 && n < 100)
+    {
+        printf("%d", n);
+    }
+    else
+    {
+        printf("none");
+    }
+    return 0;
+}
