b20fc4fd06ef152c9fb4749f697cbdf48e011c54
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-05-14 09:09:51 -0700

    Add files via upload

---
+++ Opreators-Arithemtic-Divison.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a=10,b=3;
+    int q,r;
+    float f1=22;
+    float f2=2;
+    float ans;
+
+    q=a/b; //quotients 
+    r=a%b;  //reminder
+    ans=f1/f2; //ans or result 
+    printf("\n %d",q);
+    printf("\n %d",r);
+    printf("\n %d",ans);
+
+  return 0;
+}
